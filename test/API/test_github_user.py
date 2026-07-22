7b69c6ee15c9710aa98f36ca5b00e9b863a4eac7
felipe <anfefu1@gmail.com>
2026-07-21 20:09:16 -0500

    first happy path test created

--- test/API/test_github_user.py
+++ test/API/test_github_user.py
@@ -20,7 +20,7 @@ def test_get_valid_user_profile(github_api_client):
     username = "octocat" #github demo account
     
     # Act. Executes the single action
-    response = github_api_client.get_method(f'/users/{username}')
+    response = github_api_client.get_method(f'/{username}')
 
     # Assert. Verifies the outcome 
     
@@ -36,6 +36,35 @@ def test_get_valid_user_profile(github_api_client):
     
     # Verifies if public_repos is in the response body
     assert 'public_repos' in response.body
+    
+@pytest.mark.api_test
+def test_get_user_repos(github_api_client):
+    # Fetch a user's public repositories
+    # Expected 200 OK with list of repos
+    
+    # Arrange
+    username = "octocat"
+    
+    # Act
+    response = github_api_client.get_method(f'/{username}')
+    
+    # Assert
+    assert response.is_success
+    
+    # Verifies that the JSON response was succesfully parsed into a Python list
+    assert isinstance(response.body, list) # Expected a list of repositories
+    
+    # List isn't empty
+    assert len(response.body) > 0 # Expected at least one repository
+    
+    
+# ------------------------------------------------------------------
+# NEGATIVE TEST CASES SAD PATH XD
+# ------------------------------------------------------------------
+
+    
+    
+    
     
 
     
